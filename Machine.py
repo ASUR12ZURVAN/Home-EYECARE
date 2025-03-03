da8cf6409d75c0ffdc7891e044acfe3b1f634d05
WAKE-UP-TO-REALITY <asishkumar54035@gmail.com>
2025-03-03 20:17:29 +0530

    printing the data set

--- Machine.py
+++ Machine.py
@@ -2,4 +2,5 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data =pd.read_csv("vision_readability_3m (1)")
+data =pd.read_csv("vision_readability_3m (1).csv")
+print(data)

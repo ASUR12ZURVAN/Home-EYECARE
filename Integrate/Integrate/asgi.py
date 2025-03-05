8f9944b908ed15ca551772966cf6a38a1e749902
ASUR12ZURVAN <143814906+ASUR12ZURVAN@users.noreply.github.com>
2025-03-05 16:11:34 +0530

    Backend DATABASE

--- Integrate/Integrate/asgi.py
+++ Integrate/Integrate/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Integration project.
+ASGI config for Integrate project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Integration.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Integrate.settings')
 
 application = get_asgi_application()
--- Integrate/Integrate/wsgi.py
+++ Integrate/Integrate/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Integration project.
+WSGI config for Integrate project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Integration.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Integrate.settings')
 
 application = get_wsgi_application()
